3b0ab260c5d882a9fd7412e54670b52445f2601a
vadav <Rahulbeesblazor@gmail.com>
2025-09-13 13:23:43 +0530

    Initial SwiftCollab setup

---
+++ src/main/java/com/siddarthavadavalasa/SwiftCollab/SwiftCollabApplication.java
@@ -0,0 +1,13 @@
+package com.siddarthavadavalasa.SwiftCollab;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SwiftCollabApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SwiftCollabApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/siddarthavadavalasa/SwiftCollab/SwiftCollabApplicationTests.java
@@ -0,0 +1,13 @@
+package com.siddarthavadavalasa.SwiftCollab;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SwiftCollabApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
